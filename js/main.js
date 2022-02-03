b2b17f032538d52b2cc224e8adc7d5875219890d
Aexer <302801376@student.rocmondriaan.nl>
2022-02-03 13:12:12 +0100

    carousel html css af alleen nog javascript

---
+++ js/main.js
@@ -0,0 +1,14 @@
+console.log("main.js loaded");
+
+const images = document.querySelectorAll(".image");
+const btnPrev = document.querySelector(".button-prev");
+const btnNext = document.querySelector(".button-next");
+
+btnPrev.addEventListener("click", goPrevious())
+btnNext.addEventListener("click", goNext())
+
+function goPrevious() {
+}
+
+function goNext() {
+}

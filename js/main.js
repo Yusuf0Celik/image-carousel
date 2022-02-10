6199ef5b69642550c605245bbf1bb068d4cc1008
Aexer <302801376@student.rocmondriaan.nl>
2022-02-10 12:02:07 +0100

    jaavascript werkt

--- js/main.js
+++ js/main.js
@@ -4,11 +4,42 @@ const images = document.querySelectorAll(".image");
 const btnPrev = document.querySelector(".button-prev");
 const btnNext = document.querySelector(".button-next");
 
-btnPrev.addEventListener("click", goPrevious())
-btnNext.addEventListener("click", goNext())
+btnPrev.addEventListener("click", goPrevious)
+btnNext.addEventListener("click", goNext)
 
 function goPrevious() {
+  let activeImageID =  -3;
+  let nextImg;
+  for (let i = 0; i < images.length; i++) {
+      const chkImg = images[i];
+      if (chkImg.classList.contains('active')) {
+          activeImageID = i;
+          nextImg = activeImageID - 1;
+      }
+
+      if (nextImg < 0) {
+          nextImg = images.length - 1;
+      }
+  }
+  images[activeImageID].classList.remove('active');
+  images[nextImg].classList.add('active');
 }
 
 function goNext() {
+  let activeImageID =  -3;
+  let nextImg;
+  for (let i = 0; i < images.length; i++) {
+      const chkImg = images[i];
+      if (chkImg.classList.contains('active')) {
+          activeImageID = i;
+          nextImg = activeImageID + 1;
+      }
+
+      if (nextImg >= images.length) {
+          nextImg = 0;
+      }
+  }
+
+  images[activeImageID].classList.remove('active');
+  images[nxtImg].classList.add('active');
 }

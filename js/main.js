7302653fcaa118c6d8aa3aab231e6bc95817ab14
Yusuf Celik <302801376@student.rocmondriaan.nl>
2022-03-10 11:29:29 +0100

    images veranderd en js code deleted en veranderd soort van naar json object

--- js/main.js
+++ js/main.js
@@ -4,42 +4,36 @@ const images = document.querySelectorAll(".image");
 const btnPrev = document.querySelector(".button-prev");
 const btnNext = document.querySelector(".button-next");
 
-btnPrev.addEventListener("click", goPrevious)
-btnNext.addEventListener("click", goNext)
-
-function goPrevious() {
-  let activeImageID =  -3;
-  let nextImg;
-  for (let i = 0; i < images.length; i++) {
-      const chkImg = images[i];
-      if (chkImg.classList.contains('active')) {
-          activeImageID = i;
-          nextImg = activeImageID - 1;
-      }
-
-      if (nextImg < 0) {
-          nextImg = images.length - 1;
-      }
-  }
-  images[activeImageID].classList.remove('active');
-  images[nextImg].classList.add('active');
+for (let i = 0; i < images.length; i++) {
+    const showImage = images[i];
+    console.log(showImage);
 }
 
-function goNext() {
-  let activeImageID =  -3;
-  let nextImg;
-  for (let i = 0; i < images.length; i++) {
-      const chkImg = images[i];
-      if (chkImg.classList.contains('active')) {
-          activeImageID = i;
-          nextImg = activeImageID + 1;
-      }
+btnPrev.addEventListener("click", goPrevious)
 
-      if (nextImg >= images.length) {
-          nextImg = 0;
-      }
-  }
+const imagesJSON = [
+    {
+        "image": "https://images.wallpapersden.com/image/download/minimal-ship-artwork-purple-background_a2hmbmmUmZqaraWkpJRmZ21lrWxnZQ.jpg"
+    },
+    {
+        "image": "https://wallpaperaccess.com/full/2040737.jpg"
+    },
+    {
+        "image": "https://wallpaperaccess.com/full/2040740.jpg"
+    },
+    {
+        "image": "https://wallpaperaccess.com/full/2040736.jpg"
+    },
+    {
+        "image": "https://wallpaperaccess.com/full/2040743.jpg"
+    }
+];
 
-  images[activeImageID].classList.remove('active');
-  images[nextImg].classList.add('active');
+for (let i = 0; i < imagesJSON.length; i++) {
+    const myImage = imagesJSON[i]
+    // console.log(myImage.image);
+}
+
+function goPrevious() {
+    
 }

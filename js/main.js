8181f8754252a360369415ff39c3d70e83ff5354
Yusuf Celik <302801376@student.rocmondriaan.nl>
2022-03-16 09:44:42 +0100

    json array erin gezet met img element

--- js/main.js
+++ js/main.js
@@ -1,19 +1,32 @@
 console.log("main.js loaded");
 
+const imagesDiv = document.querySelector(".images");
 const images = document.querySelectorAll(".image");
 const btnPrev = document.querySelector(".button-prev");
 const btnNext = document.querySelector(".button-next");
 
-document.addEventListener("DOMContentLoaded", addImg)
-btnPrev.addEventListener("click", goPrevious)
+// btnPrev.addEventListener("click", goPrevious)
 btnNext.addEventListener("click", goNext)
+document.addEventListener("DOMContentLoaded", function() {
+    for (let i = 0; i < imagesJSON.length; i++) {
+        const myImage = imagesJSON[i];
+        const imgEl = document.createElement("img");
+        imgEl.classList.add("image");
+        imgEl.src = myImage.image;
+        imagesDiv.appendChild(imgEl);
+        if (i === 0) {
+            imgEl.classList.add("active");
+        }
+    }
+})
 
-const imagesJSON = [
-    // {
-    //     "image": "https://images.wallpapersden.com/image/download/minimal-ship-artwork-purple-background_a2hmbmmUmZqaraWkpJRmZ21lrWxnZQ.jpg"
-    // },
+const imagesJSON = 
+[
     {
-        "image": "https://wallpaperaccess.com/full/2040737.jpg"
+        "image": "https://wallpapercave.com/wp/8Os5eDI.jpg"
+    },
+    {
+        "image": "https://wallpapercave.com/wp/wp3161436.jpg"
     },
     {
         "image": "https://wallpaperaccess.com/full/2040740.jpg"
@@ -22,24 +35,12 @@ const imagesJSON = [
         "image": "https://wallpaperaccess.com/full/2040736.jpg"
     },
     {
-        "image": "https://wallpaperaccess.com/full/2040743.jpg"
+        "image": "https://wallpaperaccess.com/full/1078158.jpg"
     }
 ];
 
-function addImg() {
-    for (let i = 0; i < imagesJSON.length; i++) {
-        const myImage = imagesJSON[i];
-        const imgEl = document.createElement("img");
-        imgEl.classList.add("image");
-        imgEl.src = myImage.image;
-        console.log(myImage);
-    }
-}
-
-function goPrevious() {
-    
-}
-
 function goNext() {
-    
+    for (let i = 0; i < images.length; i++) {
+        const imageShow = images[i];
+    }
 }
